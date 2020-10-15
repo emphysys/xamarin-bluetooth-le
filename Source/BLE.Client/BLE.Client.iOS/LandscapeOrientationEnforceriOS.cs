09789a8e0e4bce73d31518974671a81485095cad
Sam Paniccia <sam.paniccia@Emphysys.com>
2020-10-15 09:52:34 -0400

    Added landscape enforcer for iOS

---
+++ Source/BLE.Client/BLE.Client.iOS/LandscapeOrientationEnforceriOS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BLE.Client.Helpers;
+using BLE.Client.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(LandscapeOrientationEnforceriOS))]
+namespace BLE.Client.iOS
+{
+
+    public class LandscapeOrientationEnforceriOS : ILandscapeOrientationEnforcer
+    {
+        public void ForceCurrentScreenLandscape()
+        {
+            UIDevice.CurrentDevice.SetValueForKey(new NSNumber((int)UIInterfaceOrientation.LandscapeLeft), new NSString("orientation"));
+        }
+
+        public void RevertToPortrait()
+        {
+            UIDevice.CurrentDevice.SetValueForKey(new NSNumber((int)UIInterfaceOrientation.Portrait), new NSString("orientation"));
+        }
+    }
+}
